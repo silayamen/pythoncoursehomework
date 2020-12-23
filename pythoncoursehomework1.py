3b0dd9f0d79007cb7de14513db7d8be8d76277bf
sıla yamen <75160207+silayamen@users.noreply.github.com>
2020-12-23 13:17:59 +0300

    Add files via upload

---
+++ pythoncoursehomework1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+name = input('Please enter first name : ')
+print("First Name "+name)
+
+name2 = input('Please enter last name : ')
+print("Last Name "+name)
+
+y= input("Please enter date of birth: ") 
+print("date of birthy" +y)
+
+age = int(input("Please enter a age: "))
+if age < 18 :
+    print("You can't go out because it's a dangereus !")
+else :
+    print("You can go out to he street. ")
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
